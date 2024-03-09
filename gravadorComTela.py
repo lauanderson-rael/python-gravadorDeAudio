7c2f5b57c9e3167481c2c1cc3292d19abe058c66
Lauanderson Rael <lauanderson38@gmail.com>
2024-03-09 09:03:16 -0300

    tela atualizada

--- gravadorComTela.py
+++ gravadorComTela.py
@@ -6,9 +6,9 @@ import tkinter as tk
 def start_recording():
     global recording
     recording = True
-    start_button.config(state=tk.DISABLED)
-    stop_button.config(state=tk.NORMAL)
-    iniciando.config(text="Gravação iniciada...!")
+    botao_iniciar.config(state=tk.DISABLED)
+    botao_parar.config(state=tk.NORMAL)
+    msg_gravacao_iniciada.config(text="Gravando...")
 
     stream = audio.open(format=pyaudio.paInt16,
                         channels=1,
@@ -21,7 +21,7 @@ def start_recording():
 def stop_recording():
     global recording
     recording = False
-    filename = "output.wav"
+    filename = "audio_capturado.wav"
     wf = wave.open(filename, 'wb')
     wf.setnchannels(1)
     wf.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
@@ -29,10 +29,10 @@ def stop_recording():
     wf.writeframes(b''.join(frames))
     wf.close()
 
-    start_button.config(state=tk.NORMAL)
-    stop_button.config(state=tk.DISABLED)
-    iniciando.config(text="")
-    confirmacao.config(text="Gravação concluída com sucesso!")  # Define o texto como "Gravação concluída" após parar a gravação
+    botao_iniciar.config(state=tk.NORMAL)
+    botao_parar.config(state=tk.DISABLED)
+    msg_gravacao_iniciada.config(text="")
+    msg_gravacao_finalizada.config(text="Gravação concluída com sucesso!")  # Define o texto como "Gravação concluída" após parar a gravação
     
 
 def _record(stream):
@@ -48,18 +48,17 @@ audio = pyaudio.PyAudio()
 
 # Tela
 janela = tk.Tk()
-janela.title("GRAVADOR DE ÁUDIO")
+janela.title("GRAVADOR DE ÁUDIO - PYTHON")
 janela.geometry("400x250")  # Definindo o tamanho da janela
 
-start_button = tk.Button(janela, text="INICIAR GRAVAÇÃO", command=start_recording)
-start_button.pack()
-iniciando = tk.Label(janela, text="" , padx=10, pady=10)
-iniciando.pack()
+botao_iniciar = tk.Button(janela, text="INICIAR GRAVAÇÃO", command=start_recording)
+botao_iniciar.pack()
+msg_gravacao_iniciada = tk.Label(janela, text="" , padx=10, pady=10)
+msg_gravacao_iniciada.pack()
 
-stop_button = tk.Button(janela, text="PARAR E SALVAR GRAVAÇÃO", command=stop_recording, state=tk.DISABLED)
-stop_button.pack()
-
-confirmacao = tk.Label(janela, text="" , padx=10, pady=10)
-confirmacao.pack()
+botao_parar = tk.Button(janela, text="PARAR E SALVAR GRAVAÇÃO", command=stop_recording, state=tk.DISABLED)
+botao_parar.pack()
+msg_gravacao_finalizada = tk.Label(janela, text="" , padx=10, pady=10)
+msg_gravacao_finalizada.pack()
 
 janela.mainloop()
